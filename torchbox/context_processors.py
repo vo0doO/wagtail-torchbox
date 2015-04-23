e82f08552adc4e9919f0ca32f772cbdcfb8e25ae
Glenn Barr <glenn@torchbox.com>
2015-04-23 16:38:42 +0100

    Remove unneeded request.method

--- torchbox/context_processors.py
+++ torchbox/context_processors.py
@@ -18,18 +18,6 @@ def contact_form(request):
     form_params = form_page.get_form_parameters()
     form = form_class(**form_params)
 
-    if request.method == 'POST':
-        if request.POST.get('action', None) == 'contact-us':
-            form = form_class(request.POST, **form_params)
-
-            if form.is_valid():
-                form_page.process_form_submission(form)
-
-                # If we have a form_processing_backend call its process method
-                if hasattr(form_page, 'form_processing_backend'):
-                    form_processor = form_page.form_processing_backend()
-                    form_processor.process(form_page, form)
-
     for field in form:
         field.field.widget.attrs['placeholder'] = field.help_text
 
