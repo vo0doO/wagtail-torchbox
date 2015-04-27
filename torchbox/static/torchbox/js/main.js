5e0ab006ecebd9bd6373ac071fd4c4676e293353
Glenn Barr <glenn@torchbox.com>
2015-04-27 17:14:59 +0100

    Show/hide functionality on footer form

--- torchbox/static/torchbox/js/main.js
+++ torchbox/static/torchbox/js/main.js
@@ -78,7 +78,7 @@ $(function() {
     $('.pick').each(function(){
         var $stickyEl = $('.pick');
         var elTop = $stickyEl.length && $stickyEl.offset().top - 4;
-        
+
         $stickyEl.toggleClass('fixed', $window.scrollTop() > elTop);
 
         $window.scroll(function() {
@@ -89,7 +89,7 @@ $(function() {
     $('.nextprev').each(function(){
         var $stickyEl = $('.nextprev');
         var elTop = $stickyEl.length && $stickyEl.offset().top - 10;
-        
+
         $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
 
         $window.scroll(function() {
@@ -99,7 +99,7 @@ $(function() {
 
     // fitVids
     $('.responsive-object').fitVids();
-    
+
     // get some stats about the video
     $('.responsive-object').each(function() {
         var $this = $(this);
@@ -110,7 +110,7 @@ $(function() {
             player.api('play');
         });
 
-        player.addEvent('ready', function() {  
+        player.addEvent('ready', function() {
             player.addEvent('play', function(id){
                 $('.play').hide();
                 $('.breakpoint-medium header, .breakpoint-medium .hero-video h1').animate({
@@ -118,13 +118,13 @@ $(function() {
                 }, 500, function() {});
                 $('.poster').fadeOut(300);
                 ga('send', 'event', 'video', 'played', id);
-            }); 
+            });
             player.addEvent('pause', function(id){
                 $('header').animate({
                     opacity: 1,
                 }, 500, function() {});
                 ga('send', 'event', 'video', 'paused', id);
-            });     
+            });
             player.addEvent('finish', function(id){
                  $('header').animate({
                     opacity: 1,
@@ -134,7 +134,14 @@ $(function() {
             });
         });
     });
-    
+
     //featherlight used for lightboxes in streamfield-enabled pages
     $('.gallery').featherlightGallery();
+
+    // Animate contact form in footer
+    $('.contact-form').hide();
+    $('.footerbleed a[href^="/contact"]').click(function(e) {
+        e.preventDefault();
+        $('.contact-form').slideToggle();
+    });
 });
